77a745893f808656dbb3f0a14b80be87cc7de4f4
jonathanko500 <jkko9010@gmail.com>
2019-03-07 22:24:55 -0800

    assignment 2
    all starter code in

--- Assignment 2/src/simulationFramework.java
+++ Assignment 2/src/simulationFramework.java
@@ -4,4 +4,50 @@ public class simulationFramework
 {//start
 	private int currentTime;
 	private Queue<Event> eventQ = new PriorityQueue<>();
+	//constructor
+	public simulationFramework()
+	{
+		this.currentTime=0;
+		eventQ.clear();
+	}
+	public simulationFramework(int time, Queue<Event> x)
+	{
+		this.currentTime=time;
+		Object[] cop = x.toArray();
+		for(int i=0;i<x.size();i++)
+		{
+			eventQ.add((Event)cop[i]);
+		}
+	}
+	//setter
+	public void setCurrentTime(int x)
+	{
+		currentTime=x;
+	}
+	public void setEvent(Queue<Event> x)
+	{
+		Object[] copy = x.toArray();
+		for(int i=0;i<x.size();i++)
+		{
+			eventQ.add((Event)copy[i]);
+		}
+	}
+	//getter
+	public int getCurrentTime()
+	{
+		return currentTime;
+	}
+	public Queue<Event> getEventQ()
+	{
+		return eventQ;
+	}
+	//methods
+	public void schduleEvent(ArriveEvent x)
+	{
+		
+	}
+	public void run()
+	{
+		
+	}
 }//end
