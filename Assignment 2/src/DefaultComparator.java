7c74555c7870a6f4bd8e0c43fd7c08a60bfb47ee
jonathanko500 <jkko9010@gmail.com>
2019-03-11 23:19:36 -0700

    assignment 2
    more starter code

---
+++ Assignment 2/src/DefaultComparator.java
@@ -0,0 +1,10 @@
+
+public class DefaultComparator implements java.util.Comparator{
+
+	@Override
+	public int compare(Object left, Object right) {
+		
+		return 0;
+	}
+
+}
---
+++ Assignment 2/src/FindMin.java
@@ -0,0 +1,6 @@
+import java.util.*;
+
+public abstract interface FindMin extends Collection
+{
+
+}
